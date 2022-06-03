d8b4317ea386ff20a8b0fdbcdfc62612cb928459
shakilahamedriaz <shakilahamedriaz@gmail.com>
2022-06-04 00:01:08 +0600

    all is done

--- Lab_4_EH_sir/problem 2.c
+++ Lab_4_EH_sir/problem 2.c
@@ -4,34 +4,47 @@ int main(){
 char ch;
 int x,y,add,mul,sub,div;
 
-printf("Enter the value of x");
-printf("Enter the value of y");
+printf("Enter the Operator  : \n");
+scanf("%c",&ch);
 
-printf("Enter the Operation: ");
-scanf("%d",&ch);
+printf("Enter the value of x :");
+scanf("%d",&x);
+printf("Enter the value of y : ");
+scanf("%d",&y);
 
 
 
 switch (ch)
 {
+
 case '+':
     add= x+y;
-    printf("a+b=%d",add);
+    printf("a+b= %d",add);
 
     break;
 
 case '-':
      sub= x-y;
-     printf("a-b=%d",sub);
+     printf("a-b= %d",sub);
 
      break;
 
 case '*':
      mul= x*y;
-     printf("")         
+     printf("x*y= %d\n",mul);
 
+     break;
+
+case '/':
+     if(x>y){
+         div=x/y;
+     }else{
+         div=y/x;
+     }
+    printf("divison is : %d\n",div);
 
 default:
+     printf("Enter a valid Operator to run your program!\n");
     break;
 }
 
@@ -39,3 +52,13 @@ default:
 
 return 0;
 }
+
+
+//output--
+//for +
+//input x=10
+//input y=5
+
+//add=15
+//sub=5
+//div=2
