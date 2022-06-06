1f9b5d7f4e06b992f2e9e8e59c6c4c6d825ae0ab
shakilahamedriaz <shakilahamedriaz@gmail.com>
2022-06-06 22:26:52 +0600

    Array

---
+++ Lab_5_EH_sir/problem 2.c
@@ -0,0 +1,43 @@
+//a program to sort an array in descending order
+#include<stdio.h>
+int main(){
+
+int a[]={35,15,25,10,5};
+int i,j,temp;
+for(i=0; i<5; i++){
+
+    printf("%d\n",a[i]);
+}
+
+for(i=0; i<5; i++){
+    for(j=i+1; j<5; j++){
+
+
+        if(a[i]<a[j]){
+
+
+            temp=a[i];
+            a[i]=a[j];
+            a[j]=temp;
+
+
+
+
+        }
+    }
+}
+
+printf("Shorted array is Ascending order : \n");
+
+for(i=0; i<5; i++)
+        printf("%d\n",a[i]);
+
+
+
+
+return 0;
+
+
+
+}
+
---
+++ Lab_5_EH_sir/problem 3.c
@@ -0,0 +1,44 @@
+//a program to find a maximum number from an array.
+
+#include<stdio.h>
+int main(){
+
+int a[]={35,15,25,10,5};
+int i,j,temp;
+for(i=0; i<5; i++){
+
+    printf("%d\n",a[i]);
+}
+
+for(i=0; i<5; i++){
+    for(j=i+1; j<5; j++){
+
+
+        if(a[i]>a[j]){
+
+
+            temp=a[i];
+            a[i]=a[j];
+            a[j]=temp;
+
+
+
+
+        }
+    }
+}
+
+printf("Maxium Number: \n");
+
+        printf("%d\n",a[4]);
+
+
+
+
+return 0;
+
+
+
+}
+
+
