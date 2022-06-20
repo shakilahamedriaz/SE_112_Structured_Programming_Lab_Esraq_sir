065e6f3fa51e7be2d8a36f5254521593727f6ac4
shakilahamedriaz <shakilahamedriaz@gmail.com>
2022-06-20 13:48:56 +0600

    updated

--- Arry_C/Str quiz3/1.c
+++ Arry_C/Str quiz3/1.c
@@ -1,1 +1,24 @@
+//1 Write a program in C to read 10 numbers from keyboard and find their sum and average
+#include <stdio.h>
+
+int main()
+{
+    int num[10],sum = 0;
+    double average;
+
+    for (int i = 0; i < 10; i++)
+    {
+        scanf("%d",&num[i]);
+        sum += num[i];
+    }
+
+    printf("Sum : %d\n",sum);
+
+    average = sum/10.0;
+
+    printf("\n Ten digit total number average: %.2lf",average);
+
+
+    return 0;
+}
 
